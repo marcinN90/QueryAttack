ccc681762daf102c0836364c6a2776e10d1c754d
marcinN90 <90.marcin.nowak@gmail.com>
2017-06-14 00:10:54 +0200

    Interfaces

---
+++ QueryAttack/Services/Attack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryAttack.Services
+{
+    class Attack : IAttack
+    {
+        public void CancelAttack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartAttack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QueryAttack/Services/IAttack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryAttack.Services
+{
+    interface IAttack
+    {
+        void StartAttack();
+
+        void CancelAttack();
+    }
+}
