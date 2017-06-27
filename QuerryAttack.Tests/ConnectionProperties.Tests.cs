f39e86a58df963bbc7ef18214f62701ea82c0101
mnowak <marcin.nowak@compwell.biz>
2017-06-27 16:44:12 +0200

    Add nUnit, first tests

---
+++ QuerryAttack.Tests/ConnectionProperties.Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using QueryAttack.Model;
+
+namespace QuerryAttack.Tests
+{
+    [TestFixture]
+    class ConnectionPropertiesTests
+    {
+		[Test]
+		public void ConnectionProperties_IsResetPropertiesSuccesfuly()
+        {
+            ConnectionProperties connProperties = new ConnectionProperties();
+            connProperties.ServerName = @"DESKTOP-SLEAS3V\SQL2014";
+            connProperties.DatabaseName = "CS";
+            connProperties.User = "sa";
+            connProperties.Password = "maca2bra";
+            connProperties.ResetProperties();
+            Assert.AreEqual(true, (connProperties.ServerName == String.Empty
+							&&	connProperties.DatabaseName == String.Empty
+							&&	connProperties.User == String.Empty
+                            &&	connProperties.Password == String.Empty));
+        }
+
+    }
+}
---
+++ QuerryAttack.Tests/ConnectionService.Tests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using QueryAttack.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerryAttack.Tests
+{
+    [TestFixture]
+    public class ConnectionServcieTest
+    {
+        [Test]
+        public void Connection_WrongConnectionProperties()
+        {
+            ConnectionService service = new ConnectionService();
+            Assert.False(service.Connect(String.Empty, String.Empty, String.Empty, String.Empty));
+        }
+        [Test]
+        public void Connection_IsNotConnectedWhenConnectionIsNull()
+        {
+            ConnectionService service = new ConnectionService();
+            Assert.False(service.IsConnected());
+        }
+    }
+}
